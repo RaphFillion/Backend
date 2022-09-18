a9caaea6af8f8f05a68a1ddb55eeb9203f9ca0aa
Raphaël Fillion <rfillion@blanko.ca>
2022-09-18 11:09:17 -0400

    Commencement cours DB

---
+++ introduction/index.js
@@ -0,0 +1,14 @@
+const http = require('http')
+
+const hostName = '127.0.0.1'
+const port = 3000
+
+const server = http.createServer((request, response)  => {
+  response.statusCode = 200
+  response.setHeader('Content-type', 'text/plain')
+  response.end('Hello world')
+})
+
+server.listen(port, hostName, () => {
+  console.log(`Server runnng at http://${hostName}:${port}`)
+})
